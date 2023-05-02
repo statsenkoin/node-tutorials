d2644908ba5a622cd7f8a0b9f89f09c94bc157a2
Ihor Statsenko <statsenkoin@gmail.com>
2023-05-02 22:32:35 +0300

    Step03 add actions to index.js to get db data

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-//  console.table(data, ['id', 'name', 'email', 'phone']);
+const contacts = require('./contacts');
 
-// console.log('listContacts() :>> ', listContacts());
-// console.log(
-//   'getContactById(contactId) :>> ',
-//   getContactById('rsKkOQUi80UsgVPCcLZZW')
-// );
-// console.log(
-//   'addContact :>> ',
-//   addContact('Vasia Pupkin', 'qwerty@mail.com', '(098) 654-32-10')
-// );
-// console.log('removeContact :>> ', removeContact('rsKkOQUi80UsgVPCcLZZW'));
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case 'list':
+      const allContacts = await contacts.listContacts();
+      return console.table(allContacts, ['id', 'name', 'email', 'phone']);
+    case 'get':
+      const contactById = await contacts.getContactById(id);
+      return console.log('contactById :>> ', contactById);
+      return;
+    case 'add':
+      const newContact = await contacts.addContact(name, email, phone);
+      return console.log('newContact :>> ', newContact);
+    case 'remove':
+      const removedContact = await contacts.removeContact(id);
+      return console.log('removedContact :>> ', removedContact);
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+};
+
+// invokeAction({ action: 'list' });
+
+// invokeAction({ action: 'get', id: 'rsKkOQUi80UsgVPCcLZZW' });
+
+// invokeAction({
+//   action: 'add',
+//   name: 'John Smith',
+//   email: 'john.smith@mail.com',
+//   phone: '(123) 222-33-44',
+// });
+
+// invokeAction({ action: 'remove', id: 'cju19uXkuGfhrUQ2zDsDd' });
